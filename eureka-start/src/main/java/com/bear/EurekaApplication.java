ee409dca33e021ca17c0fd6cc2a0b83a01e59a1a
Bear <995748222@qq.com>
2020-05-18 22:45:16 +0800

    Eureka-server  init

---
+++ eureka-start/src/main/java/com/bear/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.bear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author XiaoXiong.Li
+ * @Date 22:41 2020/5/18
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
